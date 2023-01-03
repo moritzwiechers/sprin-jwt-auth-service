fb17584555f44f99b688dd17358508a83b81c211
Moritz Wiechers <wiechers@iaw.rub.de>
2023-01-03 18:25:54 +0100

    init

---
+++ src/test/java/de/gemorra/springjwt/SpringJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package de.gemorra.springjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
